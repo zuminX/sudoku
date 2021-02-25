235a710d1275c00ec933ea5f1bb448c353d7da8e
zuminX <1215287120@qq.com>
2021-02-25 21:54:12 +0800

    修复系统模块中无法将前端的日期字符串转为对应的统计日期对象的Bug

--- sudoku-framework/src/main/java/com/sudoku/framework/handler/GlobalDataBindingHandler.java
+++ sudoku-framework/src/main/java/com/sudoku/framework/handler/GlobalDataBindingHandler.java
@@ -1,11 +1,12 @@
 package com.sudoku.framework.handler;
 
+import com.sudoku.common.constant.enums.StatisticsDate;
+import com.sudoku.common.core.domain.CustomEditorInfo;
 import com.sudoku.common.core.handler.DataBindingHandler;
+import java.util.Collections;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
-
-import java.util.ArrayList;
 
 /**
  * 全局数据绑定处理器类
@@ -21,7 +22,16 @@ public class GlobalDataBindingHandler implements DataBindingHandler {
   @Override
   @InitBinder
   public void initBinder(WebDataBinder binder) {
-    initBinder(binder, new ArrayList<>());
+    initBinder(binder, Collections.singletonList(getStatisticsDateEditor()));
+  }
+
+  /**
+   * 获取统计日期的自定义编辑信息对象
+   *
+   * @return 统计日期的自定义编辑信息对象
+   */
+  private CustomEditorInfo<StatisticsDate> getStatisticsDateEditor() {
+    return new CustomEditorInfo<>(StatisticsDate.class, StatisticsDate::findByName);
   }
 
 }
--- sudoku-game/src/main/java/com/sudoku/game/controller/GameBaseController.java
+++ sudoku-game/src/main/java/com/sudoku/game/controller/GameBaseController.java
@@ -3,7 +3,6 @@ package com.sudoku.game.controller;
 import com.sudoku.common.constant.enums.StatisticsDate;
 import com.sudoku.common.core.domain.CustomEditorInfo;
 import com.sudoku.common.core.handler.DataBindingHandler;
-import com.sudoku.game.enums.RankingType;
 import com.sudoku.game.model.entity.SudokuLevel;
 import com.sudoku.game.service.SudokuLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +41,6 @@ public class GameBaseController implements DataBindingHandler {
   private List<CustomEditorInfo<?>> getCustomEditorInfoList() {
     List<CustomEditorInfo<?>> customEditorInfoList = new ArrayList<>();
     customEditorInfoList.add(getSudokuLevelEditor());
-    customEditorInfoList.add(getRankingTypeEditor());
     customEditorInfoList.add(getStatisticsDateEditor());
     return customEditorInfoList;
   }
@@ -54,15 +52,6 @@ public class GameBaseController implements DataBindingHandler {
    */
   private CustomEditorInfo<SudokuLevel> getSudokuLevelEditor() {
     return new CustomEditorInfo<>(SudokuLevel.class, text -> sudokuLevelService.getSudokuLevel(Integer.parseInt(text)));
-  }
-
-  /**
-   * 获取排行类型的自定义编辑信息对象
-   *
-   * @return 排行类型的自定义编辑信息对象
-   */
-  private CustomEditorInfo<RankingType> getRankingTypeEditor() {
-    return new CustomEditorInfo<>(RankingType.class, RankingType::findByName);
   }
 
   /**
