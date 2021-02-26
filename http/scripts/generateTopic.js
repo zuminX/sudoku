6f4a0fc76e51114da7727970d3cf045dbab53be8
zuminX <1215287120@qq.com>
2021-02-26 20:17:31 +0800

    添加HTTP测试

---
+++ http/scripts/generateTopic.js
@@ -0,0 +1,11 @@
+client.test("请求成功执行", function () {
+  client.assert(response.status === 200, "响应状态不为200");
+  var body = response.body;
+  client.assert(body.success === true, "请求结果中是否成功字段不为true");
+  client.assert(body.httpStatus === 200, "请求结果中http请求状态字段不为200");
+});
+
+if (response.status === 200) {
+  client.log(JSON.stringify(response.body.data.matrix))
+  client.global.set("sudokuMatrix", JSON.stringify(response.body.data.matrix))
+}
